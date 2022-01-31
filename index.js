80876dda6641837254199dd184ef600c5ff1b1bf
Dean <deanscottg@gmail.com>
2022-01-31 09:57:01 -0500

    commit tree ca415da7c30d95524c06819a1c5622585cea2681

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
+  if (rideDistance < 400)
+  {return "This one is on me!"};
+  if (rideDistance < 2500)
+  {return "I will gladly take your thirty bucks."};
+  if (rideDistance > 2500);{return "No can do."};
+  // Write your code here!
+
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good.":"No go.";
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous': 
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  };
 }
